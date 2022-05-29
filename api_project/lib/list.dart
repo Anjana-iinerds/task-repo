f87caa61a2a22c8546458669a121f0b1d19fdf2d
Anjana Satheesh <anjana.s@iinerds.com>
2022-05-29 21:25:07 +0530

    sample code

--- api_project/lib/list.dart
+++ api_project/lib/list.dart
@@ -1,89 +1,50 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
-class listpage extends StatefulWidget {
-  const listpage({super.key});
-
-  @override
-  State<listpage> createState() => _listpageState();
-}
-
-class _listpageState extends State<listpage> {
-  
-  void getData() async{
-
-    var data = await  http.get(Uri.https('jsonplaceholder.typicode.com', 'users'));
-
-    var jsonData =json.decode(data.body);
-    
-  @override
-  Widget build(BuildContext context) {
-
-        return Container(
-          child: FutureBuilder(builder: (BuildContext context, AsyncSnapshot snapshot){
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (BuildContext context, int index){
-                return Padding(
-                  padding: EdgeInsets.symmetric(vertical: 1.0,horizontal: 4.0),
-                  child: Card(
-                    child: ListTile(
-                      title: Text(snapshot.data[index].name),
-                      leading:Text(snapshot.data[index].username),
-                      trailing: Text(snapshot.data[index].email),
-                    ),
-                  ),
-                  );
-              }
-            );
-              },
-            ),
-        );
-          }
-    }
-      @override
-       Widget build(BuildContext context) {
-        return Container();
-      }
-      }
 class Users{
-  String name;
-  String username;
-  String email;
-  List<Address> address;
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final Address address;
+  final String phone;
+  final Company company;
 
-  Users({required this.name, required this.username, required this.email, required this.address});
-
-  Users.fromJson(Map<String, dynamic> usersJson)
-  : name = usersJson['name'],
-  username = usersJson['username'],
-  email = usersJson['email'],
-  address = List.from(usersJson['address'])
-  .map((address) => Address.fromJson(address)).toList();
+  Users({required this.id, required this.name, required this.username, required this.email, required this.address, required this.phone, required this.company});
+  factory Users.fromJson(Map<String, dynamic> json){
+    return  Users(
+      id: json['id'],
+      name: json['name'],
+      username: json['username'],
+      email: json['email'],
+      address: Address.fromJson(json['address']),
+      phone: json['phone'],
+      company:Company.fromJson(json['company'])
+    );
+  }
 }
 class Address{
-  String street;
-  String suite;
-  String city;
-  String zipcode;
-  Company company;
+  final String street;
+  final String suite;
+  final String city;
 
-  Address.fromJson(Map<String, dynamic> addressJson)
-  :street =addressJson['street'],
-   suite = addressJson['suite'],
-   city = addressJson['city'],
-   zipcode = addressJson['zipcode'],
-   company = Company.fromJson(addressJson['company']);
+  Address({required this.street, required this.suite, required this.city});
+  factory Address.fromJson(Map<String, dynamic> json){
+    return Address(
+      street: json['street'],
+      suite: json['suite'],
+      city: json['city']
+    );
+  }
 }
 class Company{
-  String name;
-  String catchPhrase;
-  String bs;
+  final String name;
+  final String catchPhrase;
+  final String bs;
 
-  Company.fromJson(Map<String, dynamic> companyJson)
-  :name = companyJson['name'],
-   catchPhrase = companyJson['catchPhrase'],
-   bs = companyJson['bs'];
+  Company({required this.name, required this.catchPhrase, required this.bs});
+  factory Company.fromJson(Map<String, dynamic> json){
+    return Company(
+      name: json['name'],
+      catchPhrase: json['catchPhrase'],
+      bs: json['bs']
+    );
+  }
 }
